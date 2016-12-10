e420868d3d821fbee442bcbf7c6c230fd89c297f
Arkadiusz Kurylowicz <arkadiusz@kurylowicz.info>
2016-12-10 13:25:38 +0100

    SSDP tweeks

--- last.js
+++ last.js
@@ -5,10 +5,11 @@ var crypto = require('crypto');
 var lastFmApi = require('lastfmapi');
 fs = require('fs');
 
+var ssdpClient;
 var devices = [];
-var current_songs = [];
-var last_fm_client;
-var last_fm_session;
+var currentSongs = [];
+var lastFmClient;
+var lastFmSession;
 
 function initLastFmClient(onSuccess) {
   fs.readFile('./secret.json', 'utf8', function (err, session) {
@@ -16,7 +17,7 @@ function initLastFmClient(onSuccess) {
       console.log('Config secret.json is missing, please create one');
     } else {
       var secrets = JSON.parse(session);
-      last_fm_client = new lastFmApi({'api_key': secrets.key, 'secret': secrets.secret});
+      lastFmClient = new lastFmApi({'api_key': secrets.key, 'secret': secrets.secret});
       onSuccess();
     }
   });
@@ -25,19 +26,19 @@ function initLastFmClient(onSuccess) {
 function authenticateLastFmUser(onSuccess) {
   fs.readFile('./session.json', 'utf8', function (err, session) {
     if (err) {
-      last_fm_client.auth.getToken(function (err, token) {
+      lastFmClient.auth.getToken(function (err, token) {
         console.log('Go to url: http://www.last.fm/api/auth/?api_key=' + lastFmApiKey + '&token=' + token + ' give application access and then press any key to continue');
         process.stdin.setRawMode();
         process.stdin.resume();
         process.stdin.on('data', function () {
-          last_fm_client.auth.getSession(token, function (err, newSession) {
+          lastFmClient.auth.getSession(token, function (err, newSession) {
             if (err) {
               console.log(err);
             } else {
               fs.writeFile('./session.json', JSON.stringify(newSession), function () {
-                last_fm_session = newSession;
+                lastFmSession = newSession;
                 console.log("Saved session");
-                console.log(last_fm_session);
+                console.log(lastFmSession);
                 onSuccess();
               });
             }
@@ -45,17 +46,17 @@ function authenticateLastFmUser(onSuccess) {
         });
       });
     } else {
-      last_fm_session = JSON.parse(session);
+      lastFmSession = JSON.parse(session);
       console.log("Restored session");
-      console.log(last_fm_session);
+      console.log(lastFmSession);
       onSuccess();
     }
   });
 }
 
 function sendScrobble(song) {
-  last_fm_client.setSessionCredentials(last_fm_session.name, last_fm_session.key);
-  last_fm_client.track.scrobble({
+  lastFmClient.setSessionCredentials(lastFmSession.name, lastFmSession.key);
+  lastFmClient.track.scrobble({
     'artist': _.get(song, 'artist.name'),
     'track': _.get(song, 'name'),
     'timestamp': Math.floor((new Date()).getTime() / 1000)
@@ -88,7 +89,7 @@ function getProperSong(input, artist, album, track, onSuccess, onError) {
   if (!artist || !track || artist.length == 0 || track.length == 0) {
     onError('Missing artist or title - unable to identify a song');
   } else {
-    last_fm_client.track.getInfo({'artist': artist, 'track': track}, function (err, track) {
+    lastFmClient.track.getInfo({'artist': artist, 'track': track}, function (err, track) {
       if (err) {
         onError(err);
       } else {
@@ -124,25 +125,25 @@ function getSong(ip, input, onReady) {
               var artist = _.trim(_.get(parsed, 'artist', '')), album = _.trim(_.get(parsed, 'album', '')), track = _.trim(_.get(parsed, 'track', ''));
             }
 
-            var current = _.get(current_songs, ip);
+            var current = _.get(currentSongs, ip);
             var md5sum = crypto.createHash('md5').update(artist + album + track).digest('hex');
 
             if (current == null || current.checksum != md5sum) {
               var funcSongFound = function (song) {
                 console.log('Got ', [_.get(song, 'artist.name'), _.get(song, 'album.title', ''), _.get(song, 'name')], ' - waiting 10s before scrobble');
-                _.set(current_songs, [ip], {checksum: md5sum, count: 0, scrobbled: false, song: song});
+                _.set(currentSongs, [ip], {checksum: md5sum, count: 0, scrobbled: false, song: song});
               };
               var funcSongNotFound = function (err) {
                 console.log('Not scrobbling:', err);
-                _.set(current_songs, [ip], {checksum: md5sum, count: 0, scrobbled: true, song: null});
+                _.set(currentSongs, [ip], {checksum: md5sum, count: 0, scrobbled: true, song: null});
               };
               getProperSong(input, artist, album, track, funcSongFound, funcSongNotFound);
             }
             if (current != null && current.checksum == md5sum) {
-              _.set(current_songs, [ip, 'count'], (current.count + 1));
+              _.set(currentSongs, [ip, 'count'], (current.count + 1));
             }
             if (current != null && current.checksum == md5sum && current.count > 10 && current.scrobbled == false && current.song != null) {
-              _.set(current_songs, [ip, 'scrobbled'], true);
+              _.set(currentSongs, [ip, 'scrobbled'], true);
               onReady(current.song);
             }
           }
@@ -180,31 +181,44 @@ function playInfoLoop() {
   });
 }
 
-function ssdpSearchLoop() {
-  var ssdpClient = new ssdp();
-  devices = [];
-  ssdpClient.on('response', function inResponse(headers, code, rinfo) {
-    var model = _.get(headers, 'X-MODELNAME');
-    if (model && (model.indexOf('CRX') !== -1 || model.indexOf('WX') !== -1)) {
+function initSsdpClient(onReady) {
+  ssdpClient = new ssdp();
+  ssdpClient.on('response', processSsdpResponse);
+  setTimeout(onReady, 500);
+}
+
+function processSsdpResponse(headers, code, rinfo) {
+  var model = _.get(headers, 'X-MODELNAME');
+  if (model && (model.indexOf('CRX-') !== -1 || model.indexOf('WX-') !== -1)) {
+    if (_.indexOf(devices, rinfo['address']) == -1) {
       devices.push(rinfo['address']);
     }
-    console.log('Current devices:');
-    console.log(devices);
-  });
-  ssdpClient.search('urn:schemas-upnp-org:device:MediaRenderer:1');
+  }
 }
 
-function run() {
-  // loop sor SSDP device discovery
-  ssdpSearchLoop();
-  setInterval(ssdpSearchLoop, 1000 * 60);
-
-  // loop for scrobbler
-  initLastFmClient(function () {
-    authenticateLastFmUser(function () {
-      setInterval(playInfoLoop, 1000);
-    })
-  });
+function ssdpSearch() {
+  ssdpClient.search('ssdp:all');
+  setTimeout(function () {
+    ssdpClient.stop();
+    console.log('Devices in network:', devices);
+  }, 2000);
 }
 
-run();
+// loop SSDP device discovery every 1 minute
+initSsdpClient(function () {
+  ssdpSearch();
+  setInterval(ssdpSearch, (1000 * 60));
+});
+
+//  expire device list every 15 minutes
+setInterval(function () {
+  console.log('Resetting devices list');
+  devices = [];
+}, (1000 * 60 * 15));
+
+// login user info a Last.FM and loop current song check/scrobble every 1s
+initLastFmClient(function () {
+  authenticateLastFmUser(function () {
+    setInterval(playInfoLoop, 1000);
+  })
+});
